ba8f90e496c2ba67dd4525e08f19b5440ac89582
Michael Turquette <mturquette+renesas@baylibre.com>
2016-03-13 19:48:02 -0700

    sched/cpufreq: pass sched class into cpufreq_update_util
    cpufreq_update_util() accepts a single utilization value which  does not
    account for multiple utilization contributions from the cfs, rt & dl
    scheduler classes. Begin fixing this by adding a sched_class argument to
    cpufreq_update_util(), all of its call sites and the governor-specific
    hooks in intel_pstate.c, cpufreq_schedutil.c and cpufreq_governor.c.
    
    A follow-on patch will add summation of the sched_class contributions to
    the schedutil governor.
    
    Signed-off-by: Michael Turquette <mturquette+renesas@baylibre.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2867,7 +2867,7 @@ static inline void update_load_avg(struct sched_entity *se, int update_tg)
 		 * thread is a different class (!fair), nor will the utilization
 		 * number include things like RT tasks.
 		 */
-		cpufreq_update_util(rq_clock(rq), min(cap, max), max);
+		cpufreq_update_util(cfs_util, rq_clock(rq), min(cap, max), max);
 	}
 }
 
